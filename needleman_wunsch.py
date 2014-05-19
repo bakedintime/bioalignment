5f3f312906a0b3dfba017af22312fa72d40f3415
Joel Cantoral <jcantoral@tir.com.gt>
2014-05-19 12:48:48 -0600

    Reestructura de la clase needleman_wunsch y no puede nombrarse con -

---
+++ needleman_wunsch.py
@@ -0,0 +1,26 @@
+#import nwalign as nw
+import random
+
+class FastaReader(object):
+    def get_sequence(self, filename):
+        # Read & Format
+        #for file_name in fasta_files:
+        f = open(filename, "r")
+        flines = f.readlines()
+        seq = ""
+        for el in flines[1:]:
+            seq += el.strip()
+        f.close()
+        return seq
+
+class NWAligner(object):
+
+    def align_sequences(self, seq1, seq2):
+        #result = nw.global_align(seq1, seq2, matrix='BLOSUM62.txt')
+        #return result
+        return 1
+
+    def score_alignment(self, seq1, seq2, gp_e=-2, gp_o=-5):
+        #score = nw.score_alignment(seq1, seq2, matrix='BLOSUM62.txt', gap_extend=gp_e, gap_open=gp_o)
+        score = random.randint(1,735)
+        return score
