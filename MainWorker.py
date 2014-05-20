ea303521812c580f25a3765424e8a770460e37b9
Joel Cantoral <cs.joel.09@gmail.com>
2014-05-20 00:15:13 -0600

    Se agrego el modulo de algoritmo genético, se obliga al llenado de la matriz a esperar hasta mostrar los resultados

--- MainWorker.py
+++ MainWorker.py
@@ -1,4 +1,6 @@
 import glob, os
+import pymongo
+from GenAlgorithm import DNA, Population
 from BioMatrix import BioMatrix
 from DistributedWorker import compute_two_sequences
 from needleman_wunsch import FastaReader
@@ -9,32 +11,100 @@ class MainWorker(object):
         # Inicializar la matriz y subirla a mongodb
         self.biom = BioMatrix()
         self.biom.init_db()
+        self.biom.init_contigs(len(self.fasta_files))
         if overwrite:
             self.biom.init_matrix()
             m = self.biom.get_matrix()
             self.biom.post_to_db(m)
-        else: 
+        else:
             self.biom.get_from_db()
-        self.biom.init_contigs(len(self.fasta_files))
         self.fastaReader = FastaReader()
+        self.sequences = []
+
+    def load_sequences(self):
+        for f1 in self.fasta_files:
+            seq = self.fastaReader.get_sequence(f1)
+            self.sequences.append([seq, f1])
+
+    def build_genetic_alg(self):
+        #def gen_func(dna):
+        #    """Used to initialise DNA. Should return a single gene - in this case,
+        #    a letter"""
+        #    return random.choice(string.letters + ' ,.?')
+
+        def eval_func(dna):
+            """Used to evaluate fitness of DNA. Should return a fitness value, 0-1
+            where higher is better."""
+            score = 0
+            for index, gene in enumerate(dna.genes):
+                if gene == target[index]:
+                    score += 1
+            fitness = score / float(len(target))
+            return fitness
+
+        def mut_func(dna):
+            """Used to mutate a gene. Should return a single gene."""
+            return random.choice(string.letters + ' ,.')
+
+        target = "Is Kevin a giant faglord? Yes."
+
+        self.population = Population(len(self.sequences))
+        self.population.setup(
+            #gen_func,
+            eval_func,
+            mut_func,
+            gene_length = len(target),
+            dataset = self.sequences
+        )
+
+
+
+    def check_if_finished(self):
+        conn = pymongo.Connection("mongodb://bioa:bioa@oceanic.mongohq.com:10091/bioalignment")
+        db = conn.bioalignment
+        messages = db["messages"]
+        if messages.find().count() == 0:
+            return True
+        else:
+            return False
 
     def delegate_jobs(self):
-        # total len(self.fasta_files)**2 - len(self.fasta_files)
-        for f1 in self.fasta_files:
-            for f2 in self.fasta_files:
-                if f1 != f2:
-                    seq1 = self.fastaReader.get_sequence(f1)
-                    seq2 = self.fastaReader.get_sequence(f2)
-                    result = compute_two_sequences.delay(seq1, seq2, os.path.basename(f1), os.path.basename(f2))
+        for f1 in self.sequences:
+            for f2 in self.sequences:
+                if f1[1] != f2[1]:
+                    seq1 = f1[0]
+                    seq2 = f2[0]
+                    diff = len(seq1) - len(seq2)
+                    if diff > 0:
+                        seq2 = seq2.ljust(diff+len(seq2), '-')
+                    elif diff < 0:
+                        seq1 = seq1.rjust(abs(diff)+len(seq1), '-')
+                    filename1 = f1[1]
+                    filename2 = f2[1]
+                    result = compute_two_sequences.delay(seq1, seq2, os.path.basename(filename1), os.path.basename(filename2))
                     print result
 
     def get_results(self):
+        print 'Matriz resultante:'
         print self.biom.get_from_db()
+        print 'Segmentos alineados: '
         print self.biom.get_aligned_pairs()
-        print self.biom.get_score()
+        print 'Fitness (Ponderacion de alineacion): ', self.biom.get_score()
 
 if __name__=="__main__":
-    mw = MainWorker(overwrite=False)
+    mw = MainWorker(overwrite=True)
+    mw.load_sequences()
+    #for i in range(0,5):
     mw.delegate_jobs()
-    #mw.get_results()
+    # Esperar hasta terminar los calculos
+    while True:
+        if (mw.check_if_finished()):
+            mw.get_results()
+            break
+
+        #(generation, dna) = self.population.run(1.0)
+        #print('Finished!')
+        #print('Generation: ' + str(generation))
+        #print('DNA: ' + ''.join(dna.genes))
+        #mw.get_results()
     print 'ya'
